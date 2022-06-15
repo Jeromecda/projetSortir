f12844550dc95d28f412c254c381daeb656d56e4
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-06-15 16:05:49 +0200

    FIX : DateType form sortie

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -7,8 +7,12 @@ use App\Entity\Lieu;
 use App\Entity\Participant;
 use App\Entity\Site;
 use App\Entity\Sortie;
+use DateTime;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,9 +22,15 @@ class SortieType extends AbstractType
     {
         $builder
             ->add('nom')
-            ->add('datedebut')
-            ->add('duree')
-            ->add('datecloture')
+            ->add('datedebut', DateType::class, [
+                // renders it as a single text box
+                'widget' => 'single_text',
+            ])
+            ->add('duree', NumberType::class)
+            ->add('datecloture',  DateType::class, [
+                // renders it as a single text box
+                'widget' => 'single_text',
+            ])
             ->add('nbinscriptionsmax')
             ->add('descriptioninfos')
             ->add('urlPhoto')
@@ -30,10 +40,10 @@ class SortieType extends AbstractType
             ->add('lieuNolieu', EntityType::class, 
             ['class'=>Lieu::class, 
             'choice_label'=>'nom'])
-            ->add('participants', EntityType::class, 
-            ['class'=>Participant::class, 
-            'choice_label'=>'nom',
-            'mapped' => false])
+            // ->add('participants', EntityType::class, 
+            // ['class'=>Participant::class, 
+            // 'choice_label'=>'nom',
+            // 'mapped' => false])
             ->add('siteOrganisateur', EntityType::class, 
             ['class'=>Site::class, 
             'choice_label'=>'nom'])
